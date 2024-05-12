d0ca704d4f917492c1d48ad899787c973ca07feb
maxWoosh <140332381+maxWoosh@users.noreply.github.com>
2024-05-12 20:28:02 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -4,89 +4,64 @@
 #include <cctype>
 #include <cmath>
 
-bool containsDigits(const char* word) {
-    for (int i = 0; i < strlen(word); i++) {
-        if (isdigit(word[i])) {
-            return true;
-        }
-    }
-    return false;
-}
+unsigned int faStr1(const char* str) {
+    unsigned int count = 0; // счетчик слов без цифр
 
-bool isValidWord(const char* word) {
-    if (strlen(word) == 0) {
-        return false;
-    }
-
-    for (int i = 0; i < strlen(word); i++) {
-        if (!isalpha(word[i])) {
-            return false;
-        }
-    }
-    return true;
-}
-
-
-unsigned int faStr1(const char *str) {
-    unsigned int count = 0;
-
-    const char* delim = " ";
-    char* token = strtok(const_cast<char*>(str), delim);
-
-    while (token != NULL) {
-        if (!containsDigits(token) && isValidWord(token)) {
+    while (*str) {
+        if (isalpha(*str)) {
+            while (isalnum(*str)) {
+                if (isdigit(*str)) {
+                    break;
+                }
+                str++;
+            }
             count++;
+        } else {
+            str++;
         }
-        token = strtok(NULL, delim);
     }
 
     return count;
 }
 
-unsigned int faStr2(const char *str) {
-    unsigned int count = 0;
+unsigned int faStr2(const char* str) {
+    unsigned int count = 0; // счетчик слов, удовлетворяющих условиям
 
-    const char* delim = " ";
-    char* token = strtok(const_cast<char*>(str), delim);
-
-    while (token != NULL) {
-        if (isupper(token[0])) {
-            bool isValid = true;
-            for (int i = 1; i < strlen(token); i++) {
-                if (!islower(token[i])) {
-                    isValid = false;
+    while (*str) {
+        if (isupper(*str)) {
+            while (isalpha(*str)) {
+                if (islower(*str)) {
+                    str++;
+                } else {
                     break;
                 }
             }
-            if (isValid) {
-                count++;
-            }
+            count++;
+        } else {
+            str++;
         }
-        token = strtok(NULL, delim);
     }
 
     return count;
 }
 
-unsigned int faStr3(const char *str) {
-    int wordCount = 0;
-    int charCount = 0;
+unsigned int faStr3(const char* str) {
+    unsigned int totalLength = 0; // общая длина слов
+    unsigned int wordCount = 0; // счетчик слов
 
-    const char* delim = " ";
-    char* token = strtok(const_cast<char*>(str), delim);
-
-    while (token != NULL) {
-        if (strlen(token) > 0) {
+    while (*str) {
+        if (isalpha(*str)) {
+            unsigned int length = 0;
+            while (isalpha(*str)) {
+                length++;
+                str++;
+            }
+            totalLength += length;
             wordCount++;
-            charCount += strlen(token);
+        } else {
+            str++;
         }
-        token = strtok(NULL, delim);
     }
 
-    if (wordCount == 0) {
-        return 0;
-    }
-    else {
-        return round((double)charCount / wordCount);
-    }
+    return totalLength / wordCount;
 }
